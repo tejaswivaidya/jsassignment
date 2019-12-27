6ead59c9e2879518f83328d48fea4410a05df11b
tejaswivaidya <tejaswivaidya30@gmail.com>
2019-12-27 22:32:51 +0530

    first assignment

---
+++ script.js
@@ -0,0 +1,36 @@
+var firstName = "Tejaswi";
+console.log(firstName);
+console.log("firstName");
+
+
+var name = "Tejaswi";
+var age = 26;
+console.log(age + 7 + name);
+console.log(name + (age + 7));
+
+console.log(7 + 8);
+console.log(7 - 8);
+console.log(7 * 8);
+console.log(7/8);
+console.log(7%8);
+console.log(7 > 8);
+console.log(7 < 8);
+
+var isFriday = true;
+console.log(isFriday)
+
+if(isFriday){
+    console.log("Today is Friday");
+}
+else
+{
+    console.log("Today is Tuesday");
+}
+var isFriday = false;
+if(isFriday){
+    console.log("Today is Friday");
+}
+else
+{
+    console.log("Today is Tuesday");
+}
